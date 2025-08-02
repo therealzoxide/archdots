24d199ff19ed940e8105f5affe2682a288a43a99
ayaya <was1014@proton.me>
2025-08-02 01:06:46 -0500

    >

--- spare_old/rofi/power.sh
+++ spare_old/rofi/power.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
     
 case "$(echo -e "Shutdown\nReboot\nLogout\nSuspend\nLock" | rofi -dmenu -theme ~/.config/rofi/themes/power.rasi -location 0 -xoffset 0 -yoffset 0 \
-    "Power:" -l 5)" in
-        Shutdown) exec systemctl poweroff;;
-        Reboot) exec systemctl reboot;;
-        Logout) exec loginctl terminate-session ${XDG_SESSION_ID-};;
-        Suspend) exec systemctl suspend;;
-        Lock) exec hyprlock;;
+	"Power:" -l 5)" in
+		Shutdown) exec systemctl poweroff;;
+		Reboot) exec systemctl reboot;;
+		Logout) exec loginctl terminate-session ${XDG_SESSION_ID-};;
+		Suspend) exec systemctl suspend;;
+		Lock) exec hyprlock;;
 esac
